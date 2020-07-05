81aeaf40b84c8fefa1d1acc027dd0705772709e7
Oleg <addicted2sounds@gmail.com>
2020-07-05 11:22:50 +0300

    add authorization (fb issues)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.hosts << ENV['VIRTUAL_HOST']
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
